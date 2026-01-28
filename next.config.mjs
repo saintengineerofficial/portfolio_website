f8a3a00451c202a0d56dad0e1bfe275adb80045b
caizhihao <caizhihao@iyinguo.com>
2026-01-28 19:38:17 +0800

    feat: 更新

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,7 @@
 const nextConfig = {
   webpack(config) {
     // Grab the existing rule that handles SVG imports
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.(".svg")
-    );
+    const fileLoaderRule = config.module.rules.find(rule => rule.test?.test?.(".svg"));
 
     config.module.rules.push(
       // Reapply the existing rule, but only for svg imports ending in ?url
@@ -35,13 +33,16 @@ const nextConfig = {
             },
           },
         },
-      }
+      },
     );
 
     // Modify the file loader rule to ignore *.svg, since we have it handled now.
     fileLoaderRule.exclude = /\.svg$/i;
 
     return config;
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
